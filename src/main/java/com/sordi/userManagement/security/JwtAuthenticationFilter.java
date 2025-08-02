8cd2d1300ef5f2aa56254d68810ac9202c4d4f56
Santiago <sordisantiago5@gmail.com>
2025-08-02 19:00:24 -0300

    remuevo import que no se utilizaba

--- src/main/java/com/sordi/userManagement/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sordi/userManagement/security/JwtAuthenticationFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
