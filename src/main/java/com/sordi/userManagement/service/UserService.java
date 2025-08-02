369fb6a7c036a855c8080c2680a1019968870ad2
Santiago <sordisantiago5@gmail.com>
2025-08-02 18:59:05 -0300

    renombro metodo "RegisterUser" a "Createuser"

--- src/main/java/com/sordi/userManagement/service/UserService.java
+++ src/main/java/com/sordi/userManagement/service/UserService.java
@@ -36,7 +36,7 @@ public class UserService {
      * @return Respuesta con los datos del usuario creado
      */
     @Transactional
-    public UserResponse registerUser(CreateUserRequest request) {
+    public UserResponse createUser(CreateUserRequest request) {
         log.info("Iniciando creación de usuario con username: {}", request.getUsername());
 
         // Validaciones con logging
