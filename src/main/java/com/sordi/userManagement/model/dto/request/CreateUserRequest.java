75fa48d4181da3c25603e5d41e5546360c7bd440
Santiago <sordisantiago5@gmail.com>
2025-08-04 23:03:33 -0300

    Actualizar expresión regular de validación de contraseña en CreateUserRequest

--- src/main/java/com/sordi/userManagement/model/dto/request/CreateUserRequest.java
+++ src/main/java/com/sordi/userManagement/model/dto/request/CreateUserRequest.java
@@ -55,7 +55,7 @@ public class CreateUserRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     @Pattern(
-        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
         message = "Password must contain at least one lowercase letter, one uppercase letter, one digit and one special character"
     )
     private String password;
